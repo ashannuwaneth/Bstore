90518654a92940fa22b519d3a7ee95ef844ab3c6
ashannuwaneth <ashannuwaneth077@gmail.com>
2022-03-11 20:33:58 +1100

    repository filr done

---
+++ Bstore.DataAccess/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Bstore.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bstore.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDataContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDataContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public void Add(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+           IQueryable<T> query=dbSet;
+            query=query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T item)
+        {
+            dbSet.Remove(item);
+        }
+
+        public void RemoveRange(IEnumerable<T> item)
+        {
+            dbSet.RemoveRange(item);
+        }
+    }
+}
