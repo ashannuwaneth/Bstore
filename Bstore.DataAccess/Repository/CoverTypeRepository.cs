8c69766b1389bddccbad5f54b1da47e42719915e
ashannuwaneth <ashannuwaneth077@gmail.com>
2022-03-12 13:05:05 +1100

    add unitofwork for covertype

---
+++ Bstore.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,24 @@
+﻿using Bstore.DataAccess.Data;
+using Bstore.DataAccess.Repository.IRepository;
+using Bstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bstore.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDataContext _db;
+        public CoverTypeRepository(ApplicationDataContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(CoverType obj)
+        {
+            _db.CoverTypes.Update(obj);
+        }
+    }
+}
