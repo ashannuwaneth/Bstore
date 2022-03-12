f49c396525fbcf2e097bc98d502ea924a59a2f57
ashannuwaneth <ashannuwaneth077@gmail.com>
2022-03-12 13:22:21 +1100

    areas completed

--- Bstore/Areas/Admin/Controllers/CategoryController.cs
+++ Bstore/Areas/Admin/Controllers/CategoryController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Learningweb.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
 
--- Bstore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bstore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
